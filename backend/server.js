38866ac354393abf9ab6e36c44a973a05b2662c1
adonl <adnlwly@gmail.com>
2022-11-29 21:15:21 +0000

    connect to mongodb with mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,33 @@
 require('dotenv').config()
 
 const express = require('express');
+const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
 
 // Express app
 const app = express();
 
 // Middelware
+app.use(express.json());
+
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
 // Routes
-app.use('/api/workouts', workoutRoutes)
+app.use('/api/workouts', workoutRoutes);
 
-// Listen for requests
-app.listen(process.env.PORT, () => {
-  console.log('Listening on PORT 3000');
-});
+// Connect to database
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+
+    // Listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log('Connected to database & listening on PORT 3000');
+    });
+
+  })
+  .catch((error) => {
+    console.log(error);
+  });
