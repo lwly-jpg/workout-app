12123d0cae4541a144b35b436784328ba544576e
adonl <adnlwly@gmail.com>
2022-11-29 20:59:00 +0000

    update routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,6 +2,29 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', () => {});
+// GET all workouts
+router.get('/', (req, res) => {
+  res.json({message: 'GET all workouts'})
+});
+
+// GET single workout
+router.get('/:id', (req, res) => {
+  res.json({message: 'GET single workout'})
+});
+
+// POST new workout
+router.post('/', (req, res) => {
+  res.json({message: 'POST new workout'})
+});
+
+// DELETE new workout
+router.delete('/:id', (req, res) => {
+  res.json({message: 'DELETE workout'})
+});
+
+// UPDATE new workout
+router.patch('/:id', (req, res) => {
+  res.json({message: 'UPDATE workout'})
+});
 
 module.exports = router;
