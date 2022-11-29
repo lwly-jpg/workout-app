e8d2207cb3a313124e6c6bb5ea80b898f8435ed2
adonl <adnlwly@gmail.com>
2022-11-29 20:55:36 +0000

    add new routes set up

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express');
+const workoutRoutes = require('./routes/workouts');
 
 // Express app
 const app = express();
@@ -12,9 +13,7 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.get('/', (req, res) => {
-  res.json({message: "Welcome!"})
-});
+app.use('/api/workouts', workoutRoutes)
 
 // Listen for requests
 app.listen(process.env.PORT, () => {
