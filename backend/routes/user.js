7c2e871b383bee22df07776b17930b81a6fb3965
adonl <adnlwly@gmail.com>
2022-11-30 20:25:02 +0000

    add user route

---
+++ backend/routes/user.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+// User login route
+router.post('/login', () => {})
+
+
+// User register route
+router.post('/register', () => {})
+
+
+
+module.exports = router;
