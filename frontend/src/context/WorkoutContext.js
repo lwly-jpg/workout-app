a249af4e3e738076f8d24e65f9a1c4611edfb4ee
adonl <adnlwly@gmail.com>
2022-11-30 06:52:12 +0000

    add workouts reducer for setting and creating workouts in workout context

--- frontend/src/context/WorkoutContext.js
+++ frontend/src/context/WorkoutContext.js
@@ -1,11 +1,29 @@
-import { createContext } from 'react'
+import { createContext, useReducer } from 'react'
 
 export const WorkoutContext = createContext()
 
+export const workoutsReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_WORKOUTS':
+      return {
+        workouts: action.payload
+      }
+    case 'CREATE_WORKOUT':
+      return {
+        workouts: [action.payload, ...state.workouts]
+      }
+    default:
+      return state
+  }
+}
+
 export const WorkoutsContextProvider = ( {children} ) => {
+  const [state, dispatch] = useReducer(workoutReducer, {
+    workouts: null
+  })
 
   return (
-    <WorkoutContext.Provider>
+    <WorkoutContext.Provider value={{state, dispatch}}>
       { children }    
     </WorkoutContext.Provider>
   )
