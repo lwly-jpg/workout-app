cd0189643c90242e5c5b325746a1424a98e88d5a
adonl <adnlwly@gmail.com>
2022-11-30 20:57:16 +0000

    add status user register method

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,17 @@ const loginUser = async (req, res) => {
 
 // User register
 const registerUser = async (req, res) => {
-  res.json({message: 'register user'})
+  const { email, password } = req.body
+
+  try {
+    const user = await User.register(email, password)
+
+    res.status(200).json({email, user})
+
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+
 }
 
 module.exports = { loginUser, registerUser }
