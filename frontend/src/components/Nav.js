50af15de4594f88aa645da32369abd49c68346da
adonl <adnlwly@gmail.com>
2022-12-01 08:50:21 +0000

    update nav component with auth conditions

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const Nav = () => {
   const { logout } = useLogout()
+  const { user } = useAuthContext()
 
   const handleClick = () => {
     logout() 
@@ -15,13 +17,20 @@ const Nav = () => {
           <h2>Workout Helper</h2>
         </Link>
         <nav>
-          <div>
-            <button onClick={handleClick}>Log out</button>
-          </div>
-          <div>
-            <Link to="/register">Register</Link>
-            <Link to="/login">Log in</Link>
-          </div>
+          {user && (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleClick}>Log out</button>
+            </div>
+          )}
+          {!user && (
+            <div>
+              <Link to="/register">Register</Link>
+              <Link to="/login">Log in</Link>
+            </div>
+
+          )}
+
         </nav>
       </div>
     </header>
