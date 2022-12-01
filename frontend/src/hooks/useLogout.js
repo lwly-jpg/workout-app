81f68d84cc99ddce8fa76e0fbb3bb23a527dcfef
adonl <adnlwly@gmail.com>
2022-12-01 07:29:22 +0000

    add use logout hook

---
+++ frontend/src/hooks/useLogout.js
@@ -0,0 +1,17 @@
+import { useAuthContext } from "./useAuthContext"
+
+export const useLogout = () => {
+  const { dispatch } = useAuthContext()
+  
+  const logout = () => {
+    // Remove user token
+    localStorage.removeItem('user')
+
+    // Dispatch logout action
+
+    dispatch({type: 'LOGOUT'})
+  }
+
+  return {logout}
+
+}
