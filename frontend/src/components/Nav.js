b394c10b5d5ad1d1e31d99bd9e2479a3882973dc
adonl <adnlwly@gmail.com>
2022-12-01 07:32:46 +0000

    update nav and styling

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -1,6 +1,12 @@
 import { Link } from 'react-router-dom'
+import { useLogout } from '../hooks/useLogout'
 
 const Nav = () => {
+  const { logout } = useLogout()
+
+  const handleClick = () => {
+    logout() 
+  }
 
   return (
     <header>
@@ -9,6 +15,9 @@ const Nav = () => {
           <h2>Workout Helper</h2>
         </Link>
         <nav>
+          <div>
+            <button onClick={handleClick}>Log out</button>
+          </div>
           <div>
             <Link to="/register">Register</Link>
             <Link to="/login">Log in</Link>
