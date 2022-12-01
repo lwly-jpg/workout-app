031532f43e0168e797a2b57743ee84adb7dfe4f0
adonl <adnlwly@gmail.com>
2022-12-01 07:38:55 +0000

    update login component to use hook

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import { useLogin } from '../hooks/useLogin'
 
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const {login, error, isLoading} = useLogin()
 
   const handleSubmit = async (event) => {
     event.preventDefault()
 
-    console.log(email, password)
+    await login(email, password)
   }
 
   return (
@@ -23,8 +25,8 @@ const Login = () => {
         type="password" 
         onChange={(event) => setPassword(event.target.value)}
       />
-      <button>Log in</button>
-
+      <button disabled={isLoading}>Log in</button>
+      {error && <div className="error">{error}</div>}
 
     </form>
   )
